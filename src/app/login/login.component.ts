4b20f625348a69d42e5ab556943d9931622108c9
unknown <karishmap29@gmail.com>
2023-01-31 22:34:45 +0530

    initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  data="your perfect banking partner"
+  inputplaceholder="account number"
+  
+  acno=''
+  // or acno:any
+  psw=''
+
+  userDetails:any={
+    1000:{acno:1000,username:"anu",password:"abc123",balance:0},
+    1001:{acno:1001,username:"amal",password:"abc123",balance:0},
+    1002:{acno:1002,username:"arun",password:"abc123",balance:0},
+    1003:{acno:1003,username:"akil",password:"abc123",balance:0},
+
+  }
+
+  constructor(){}
+  
+  ngOnInit(): void {
+
+  }
+
+  login(){
+    //  alert('login clicked')
+    var acnum=this.acno //to make the code shorter
+    var psw=this.psw
+    var userDetails=this.userDetails
+    if(acnum in userDetails){
+      if(psw==userDetails[acnum]["password"]){
+      alert("login success")
+      }
+      else{
+        alert("incorrect password!")
+      }
+    }
+    else{
+   alert("acno incorrect or not registered yet!")
+    }                                             
+  }
+  acnoChange(event:any){
+  this.acno=event.target.value;
+  //console.log(this.acno);
+   }
+   pswrdChange(event:any){
+    this.psw=event.target.value
+    //console.log(this.psw);
+    
+   }
+}
