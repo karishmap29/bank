5b2a146dd6504d3788921fc2246a061c37d7096f
unknown <karishmap29@gmail.com>
2023-01-31 23:02:33 +0530

    initial commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,14 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent { 
 
   data="your perfect banking partner"
   inputplaceholder="account number"
   
-  acno=''
+  // acno=''
   // or acno:any
-  psw=''
+  // psw=''
 
   userDetails:any={
     1000:{acno:1000,username:"anu",password:"abc123",balance:0},
@@ -28,30 +28,51 @@ export class LoginComponent {
 
   }
 
-  login(){
-    //  alert('login clicked')
-    var acnum=this.acno //to make the code shorter
-    var psw=this.psw
-    var userDetails=this.userDetails
-    if(acnum in userDetails){
-      if(psw==userDetails[acnum]["password"]){
-      alert("login success")
-      }
-      else{
-        alert("incorrect password!")
-      }
+  // login(){
+  //   //  alert('login clicked')
+  //   var acnum=this.acno //to make the code shorter
+  //   var psw=this.psw
+  //   var userDetails=this.userDetails
+  //   if(acnum in userDetails){
+  //     if(psw==userDetails[acnum]["password"]){  //acnum is a variable. so no quotes. but password is a keyword. so give in quotes.
+  //     alert("login success")
+  //     }
+  //     else{
+  //       alert("incorrect password!")
+  //     }
+  //   }
+  //   else{
+  //  alert("acno incorrect or not registered yet!")
+  //   }                                             
+  // }
+
+//   acnoChange(event:any){
+//   this.acno=event.target.value;
+//   //console.log(this.acno);
+//    }
+//    pswrdChange(event:any){
+//     this.psw=event.target.value
+//     //console.log(this.psw);
+    
+//    }
+ // }
+
+ login(a:any,b:any){
+ // console.log(a.value);
+ var acnum=a.value
+ var psw=b.value
+  
+  var userDetails=this.userDetails
+  if(acnum in userDetails){
+    if(psw==userDetails[acnum]["password"]){  //acnum is a variable. so no quotes. but password is a keyword. so give in quotes.
+    alert("login success")
     }
     else{
-   alert("acno incorrect or not registered yet!")
-    }                                             
+      alert("incorrect password!")
+    }
   }
-  acnoChange(event:any){
-  this.acno=event.target.value;
-  //console.log(this.acno);
-   }
-   pswrdChange(event:any){
-    this.psw=event.target.value
-    //console.log(this.psw);
-    
-   }
+  else{
+ alert("acno incorrect or not registered yet!")
+  }                                             
+}
 }
